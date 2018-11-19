84ae5c5737f6a1ad7dd7bc2928970c78a2e70a66
Marcus Little <marclouisiana16@gmail.com>
2018-11-18 20:47:16 -0600

    set up geocoding

--- weather-app/app.js
+++ weather-app/app.js
@@ -15,4 +15,11 @@ const argv = yargs
     .alias('help', 'h')
     .argv;
 
-geocode.geocode(argv.address)
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+    if (errorMessage) {
+        console.log(errorMessage);
+    }
+    else {
+        console.log(JSON.stringify(results, undefined, 2));
+    }
+});
--- weather-app/geocode/geocode.js
+++ weather-app/geocode/geocode.js
@@ -1,31 +1,33 @@
 const request = require('request');
 
-const geocode = (address) => {
-    var encodedAddress = encodeURIComponent(address);
+const geocodeAddress = (address, callback) => {
+    var encodedAddress = encodeURIComponent(address, callback);
     request({
         url: `http://www.mapquestapi.com/geocoding/v1/address?key=6qRJlLO0QLw2SkL1fYIrA7cAvQntyw9n&location=${encodedAddress}`,
         json: true
     }, (error, response, body) => {
         if (error) {
-            console.log("Unable to connect to server.")
+            callback("Unable to connect to server.")
         }
         else if(body.info.statuscode !== 0){
             body.info.messages.forEach(message => {
-                console.log(message)
+                callback(message)
             });
         }
         else if (body.results[0].locations[0] !== undefined){
-            console.log(`Address: ${address}`)
-            console.log(`latitude: ${body.results[0].locations[0].latLng.lat}`);
-            console.log(`longitude: ${body.results[0].locations[0].latLng.lng}`);
+            callback(undefined, {
+                address:address,
+                latitude: body.results[0].locations[0].latLng.lat,
+                longitude: body.results[0].locations[0].latLng.lng
+            })
         }
         else {
-            console.log("Address not found")
+            callback("Address not found")
         }
         
     })
 }
 
 module.exports = {
-    geocode,
+    geocodeAddress,
 }
