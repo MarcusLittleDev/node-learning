d15839fb1fed0aa11295397871f75ff9ba6fb4b2
Marcus Little <marclouisiana16@gmail.com>
2018-11-22 12:16:38 -0600

    started express server section

---
+++ node-web-server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+var app = express();
+
+app.get('/', (req, res) => {
+    // res.send('<h1>Hello Express!</h1>');
+    res.send({
+        name: 'Andrew',
+        likes: [
+            'Biking', 
+            'Cities'
+        ]
+    })
+});
+
+app.listen(8000);
