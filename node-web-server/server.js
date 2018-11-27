588173eeab6fca62734cfd7dd3757f8935daa8bb
Marcus Little <marclouisiana16@gmail.com>
2018-11-26 19:22:19 -0600

    started learning templating

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
+
+app.set('view engine', 'hbs')
+app.use(express.static(__dirname + '/public'))
 
 app.get('/', (req, res) => {
     // res.send('<h1>Hello Express!</h1>');
@@ -14,7 +18,10 @@ app.get('/', (req, res) => {
 });
 
 app.get('/about', (req, res) => {
-    res.send('About Page')
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear()
+    })
 })
 
 app.get('/bad', (req, res) => {
@@ -23,4 +30,6 @@ app.get('/bad', (req, res) => {
         fix: 'I dont know'
     })
 })
-app.listen(8000);
+app.listen(8000, () => {
+    console.log('Server is up on port 8000');
+});
